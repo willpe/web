683fd5c6e97745b0d3283312fb960c377ec046c3
Will Perry <willpe@microsoft.com>
2014-09-28 15:39:20 -0700

    move default into public

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
+
+// GET /styles.css etc
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req,res){
    res.send('hello'); 
 });
